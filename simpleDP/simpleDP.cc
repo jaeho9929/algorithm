6ff4a294af50bd661694f83d1a07e1075a6d4190
J.H. Jeong <jaeho9929@gmail.com>
2018-01-29 20:11:37 +0900

    Easy example of dynamic programming added.

---
+++ simpleDP/simpleDP.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <random>
+#include <chrono>
+#include <cassert>
+
+using namespace std;
+
+#define __MESSAGE(s)    \
+    std::cout << s << std::endl;
+
+#define __TIMEMEASURE(func, ...)      \
+    ts = std::chrono::high_resolution_clock::now();   \
+    std::cout << func(__VA_ARGS__) << std::endl;  \
+    te = std::chrono::high_resolution_clock::now();   \
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();    \
+    std::cout << "Execution time of "<< #func " " << "; " << duration << "us" << std::endl;   
+
+#define __TIMEMEASURE_NOSTDOUT(func, ...)      \
+    ts = std::chrono::high_resolution_clock::now();   \
+    func(__VA_ARGS__);  \
+    te = std::chrono::high_resolution_clock::now();   \
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();    \
+    std::cout << "Execution time of "<< #func " " << "; " << duration << "us" << std::endl;
+
+#define __TIMEMEASURE_WITHRETURN(ret ,func, ...)      \
+    ts = std::chrono::high_resolution_clock::now();   \
+    ret = func(__VA_ARGS__);  \
+    te = std::chrono::high_resolution_clock::now();   \
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();    \
+    std::cout << "Execution time of "<< #func " " << "; " << duration << "us" << std::endl;
+
+const int h = 5, w = 4;
+int dp[h+1][w+1];
+
+class simpleDP{
+    public:
+        void calc(void)
+        {
+            int i, j;
+            dp[0][0] = 1;
+            for(int i = 0; i <= h; i++){
+                for(int j = 0; j <= w; j++){
+                    if(i != 0)  dp[i][j] += dp[i - 1][j];
+                    if(j != 0)  dp[i][j] += dp[i][j - 1];
+                }
+            }
+            for(int i = 0; i <= h; i++){
+                for(int j = 0; j <= w; j++){
+                    cout << dp[i][j] << "\t";
+                }
+                cout << endl;
+            } 
+            
+        }
+};
+
+int main(void)
+{
+    //std::random_device rn;
+
+    std::chrono::high_resolution_clock::time_point ts = std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point te = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();
+    
+    simpleDP solver;
+
+    solver.calc();
+
+//    std::mt19937_64 rnd(rn());
+//    std::mt19937 rnd(rn());
+//    std::uniform_int_distribution<int> range(-10, 10);
+
+	return 0;
+}
