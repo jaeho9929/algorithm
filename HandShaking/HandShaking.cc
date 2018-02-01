beac4488841ab562984d9a38fe7664b792357159
J.H. Jeong <jaeho9929@gmail.com>
2018-02-01 10:57:21 +0900

    Handshaking program added.

---
+++ HandShaking/HandShaking.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <random>
+#include <chrono>
+#include <cassert>
+
+using namespace std;
+
+#define __MESSAGE(s)    \
+    std::cout << s << std::endl;
+
+#define __TIMEMEASURE(func, ...)      \
+    ts = std::chrono::high_resolution_clock::now();   \
+    std::cout << func(__VA_ARGS__) << std::endl;  \
+    te = std::chrono::high_resolution_clock::now();   \
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();    \
+    std::cout << "Execution time of "<< #func " " << "; " << duration << "us" << std::endl;   
+
+#define __TIMEMEASURE_NOSTDOUT(func, ...)      \
+    ts = std::chrono::high_resolution_clock::now();   \
+    func(__VA_ARGS__);  \
+    te = std::chrono::high_resolution_clock::now();   \
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();    \
+    std::cout << "Execution time of "<< #func " " << "; " << duration << "us" << std::endl;
+
+#define __TIMEMEASURE_WITHRETURN(ret ,func, ...)      \
+    ts = std::chrono::high_resolution_clock::now();   \
+    ret = func(__VA_ARGS__);  \
+    te = std::chrono::high_resolution_clock::now();   \
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();    \
+    std::cout << "Execution time of "<< #func " " << "; " << duration << "us" << std::endl;
+
+class HandShaking{
+    public:
+        long countPerfect(int n)
+        {
+            long long *dp = new long long[n/2 +1];
+
+            dp[0] = 1;
+
+            for(int i = 1; i <= n/2; i++){
+                dp[i] = 0;
+                for(int j = 0; j<i; j++){
+                    dp[i] += dp[j] * dp[i-j-1];
+                }
+            }
+
+            long long ans = dp[n/2];
+            delete[] dp;
+            return ans;
+        }
+};
+
+int main(void)
+{
+    //std::random_device rn;
+
+    std::chrono::high_resolution_clock::time_point ts = std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point te = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();
+    
+    int cc;
+    int n;
+    HandShaking solver;
+
+    cin >> cc;
+    while(cc--){
+        cin >> n;
+        __TIMEMEASURE(solver.countPerfect, n);
+    }
+
+//    std::mt19937_64 rnd(rn());
+//    std::mt19937 rnd(rn());
+//    std::uniform_int_distribution<int> range(-10, 10);
+
+	return 0;
+}
