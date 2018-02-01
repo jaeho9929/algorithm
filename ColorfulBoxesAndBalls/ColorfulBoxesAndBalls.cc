913584476388eaa91f40f470fb86ccf5a871c4f6
J.H. Jeong <jaeho9929@gmail.com>
2018-02-01 21:12:12 +0900

    ColorfulBoxesAndBalls program added.

---
+++ ColorfulBoxesAndBalls/ColorfulBoxesAndBalls.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <random>
+#include <chrono>
+#include <cassert>
+#include <limits.h>
+
+using namespace std;
+
+#define __MESSAGE(s)    \
+    std::cout << s << std::endl;
+
+#define __TIMEMEASURE(func, ...)      \
+    ts = std::chrono::high_resolution_clock::now();   \
+    std::cout << func(__VA_ARGS__) << std::endl;  \
+    te = std::chrono::high_resolution_clock::now();   \
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();    \
+    std::cout << "Execution time of "<< #func " " << "; " << duration << "us" << std::endl;   
+
+#define __TIMEMEASURE_NOSTDOUT(func, ...)      \
+    ts = std::chrono::high_resolution_clock::now();   \
+    func(__VA_ARGS__);  \
+    te = std::chrono::high_resolution_clock::now();   \
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();    \
+    std::cout << "Execution time of "<< #func " " << "; " << duration << "us" << std::endl;
+
+#define __TIMEMEASURE_WITHRETURN(ret ,func, ...)      \
+    ts = std::chrono::high_resolution_clock::now();   \
+    ret = func(__VA_ARGS__);  \
+    te = std::chrono::high_resolution_clock::now();   \
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();    \
+    std::cout << "Execution time of "<< #func " " << "; " << duration << "us" << std::endl;
+
+class ColorfulBoxesAndBalls{
+
+    public:
+        int getMaximum(int numRed, int numBlue, int onlyRed, int onlyBlue, int bothColors)
+        {
+            int ans = INT_MIN;
+            int change = min(numRed, numBlue);
+
+            for(int i = 0; i <= change; i++){
+                int myscore = (numRed - i) * onlyRed
+                                + (numBlue - i) * onlyBlue
+                                + 2*i*bothColors;
+
+                                ans = max(ans, myscore);
+            }
+            return ans;
+        }
+};
+
+int main(void)
+{
+    //std::random_device rn;
+
+    std::chrono::high_resolution_clock::time_point ts = std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point te = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();
+    
+    int cc;
+    int numRed, numBlue, onlyRed, onlyBlue, bothColors;
+
+    ColorfulBoxesAndBalls solver;
+
+    cin >> cc;
+    while(cc--){
+        cout << "CASE \"" << cc << "\"" << endl;
+        cin >> numRed >> numBlue >> onlyRed >> onlyBlue >> bothColors;
+        __TIMEMEASURE(solver.getMaximum, numRed, numBlue, onlyRed, onlyBlue, bothColors);
+    }
+
+//    std::mt19937_64 rnd(rn());
+//    std::mt19937 rnd(rn());
+//    std::uniform_int_distribution<int> range(-10, 10);
+
+	return 0;
+}
